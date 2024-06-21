c206b05ef0ec1aa143155ba7ddfd50a7f582f24f
sakzamov <semenakzamov@gmail.com>
2024-06-21 23:31:44 +0800

    3

--- main.py
+++ main.py
@@ -11,8 +11,12 @@ def go(message):
         pokemon = Pokemon(message.from_user.username)
         bot.send_message(message.chat.id, pokemon.info())
         bot.send_photo(message.chat.id, pokemon.show_img())
-    if message.from_user.username in Pokemon.pokemons.keys(3):
+    else:
         bot.reply_to(message, "Ты уже создал себе покемона")
+
+@bot.message_handler(commands=['info'])
+def info(message):
+    bot.reply_to(message, 'Покемоны (pokemon – «карманный монстр») родились из фантазий Сатоси Тадзири о том, как можно было бы обмениваться и соревноваться своими виртуальными питомцами. По легенде, эта мысль пришла к разработчику, когда он увидел жука, ползущего по проводу между приставками Game Boy. Фантастический мир покемонов поражает своим многообразием. Однако Сатоси Тадзири оказался не просто мечтателем.Покемоны — это создания всевозможных форм и размеров, живущие в природе рядом с людьми. Большинство покемонов не умеют говорить и могут лишь произнести свое имя. В данный момент вселенную покемонов населяют более 700 существ. О покемонах заботятся их владельцы, их называют')
 
 
 bot.infinity_polling(none_stop=True)
