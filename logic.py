3b09f888a2bf30757a268983d438d402bf037e3d
sakzamov <semenakzamov@gmail.com>
2024-07-05 20:15:57 +0800

    l

--- logic.py
+++ logic.py
@@ -1,5 +1,7 @@
 from random import randint
 import requests
+from datetime import datetime, timedelta
+
 
 class Pokemon:
     pokemons = {}
@@ -11,8 +13,22 @@ class Pokemon:
         self.pokemon_number = randint(1,1000)
         self.img = self.get_img()
         self.name = self.get_name()
+        self.hp = randint(100, 1000)
+        self.power = randint(1, 100)
+        self.last_feed_time = datetime.now()
+
 
         Pokemon.pokemons[pokemon_trainer] = self
+
+    def feed(self, feed_interval = 60, hp_increase = 10 ):
+        current_time = datetime.now()  
+        delta_time = timedelta(seconds=feed_interval)  
+        if (current_time - self.last_feed_time) > delta_time:
+            self.hp += hp_increase
+            self.last_feed_time = current_time
+            return f"Здоровье покемона увеличено. Текущее здоровье: {self.hp}"
+        else:
+            return f"Следующее время кормления покемона: {current_time+delta_time}"  
 
     # Метод для получения картинки покемона через API
     def get_img(self):
@@ -32,15 +48,40 @@ class Pokemon:
             return (data['forms'][0]['name'])
         else:
             return "Pikachu"
+    
+    def attack(self, enemy):
+        if isinstance(enemy, Wizard): # Проверка на то, что enemy является типом данных Wizard (является экземпляром класса Волшебник)
+            shuns = randint(1,5)
+            if shuns == 1:
+                return "Покемон-волшебник применил щит в сражении"
+        if enemy.hp > self.power:
+            enemy.hp -= self.power
+            return f"Сражение @{self.pokemon_trainer} с @{enemy.pokemon_trainer}"
+        else:
+            enemy.hp = 0
+            return f"Победа @{self.pokemon_trainer} над @{enemy.pokemon_trainer}! "
+        
 
 
     # Метод класса для получения информации
     def info(self):
-        return f"Имя твоего покеомона: {self.name}"
+        return f"Имя твоего покеомона: {self.name}, ХП: {self.hp}, Атака: {self.power}"
+
 
     # Метод класса для получения картинки покемона
     def show_img(self):
         return self.img
+class Wizard(Pokemon):
+    def feed(self):
+        return super().feed(hp_increase=25)
 
-
-
+class Fighter(Pokemon):
+    def attack(self, enemy):
+        super_power = randint(5,15)
+        self.power += super_power
+        rezylt = super().attack(enemy)
+        self.power -= super_power
+        return rezylt + f"\
+        Боец применил супер-атаку силой:{super_power} "
+    def feed(self):
+        return super().feed(feed_interval = 25)
--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import telebot 
 from config import token
-
-from logic import Pokemon
+from random import randint
+from logic import Pokemon, Wizard, Fighter
 
 bot = telebot.TeleBot(token) 
 
 @bot.message_handler(commands=['go'])
 def go(message):
     if message.from_user.username not in Pokemon.pokemons.keys():
+        chance = randint(1,3)
+        if chance == 1:
+            pokemon = Pokemon(message.from_user.username)
+        elif chance == 2:
+            pokemon = Wizard(message.from_user.username)
+        elif chance == 3:
+            pokemon = Fighter(message.from_user.username)
+        bot.send_message(message.chat.id, pokemon.info())
         pokemon = Pokemon(message.from_user.username)
         bot.send_message(message.chat.id, pokemon.info())
         bot.send_photo(message.chat.id, pokemon.show_img())
@@ -18,6 +26,32 @@ def go(message):
 def info(message):
     bot.reply_to(message, 'Покемоны (pokemon – «карманный монстр») родились из фантазий Сатоси Тадзири о том, как можно было бы обмениваться и соревноваться своими виртуальными питомцами. По легенде, эта мысль пришла к разработчику, когда он увидел жука, ползущего по проводу между приставками Game Boy. Фантастический мир покемонов поражает своим многообразием. Однако Сатоси Тадзири оказался не просто мечтателем.Покемоны — это создания всевозможных форм и размеров, живущие в природе рядом с людьми. Большинство покемонов не умеют говорить и могут лишь произнести свое имя. В данный момент вселенную покемонов населяют более 700 существ. О покемонах заботятся их владельцы, их называют')
 
+@bot.message_handler(commands=['attack'])
+def attack_pok(message):
+    if message.reply_to_message:
+        if message.from_user.username in Pokemon.pokemons.keys() and message.reply_to_message.from_user.username in Pokemon.pokemons.keys():
+            enemy = Pokemon.pokemons[message.reply_to_message.from_user.username]
+            pok = Pokemon.pokemons[message.from_user.username]
+            res = pok.attack(enemy)
+            bot.send_message(message.chat.id, res)
+        else:
+            bot.send_message(message.chat.id, "Сражаться можно только с покемонами")
+    else:
+            bot.send_message(message.chat.id, "Чтобы атаковать, нужно ответить на сообщения того, кого хочешь атаковать")
+
+@bot.message_handler(commands=['info_enemy'])
+def info_pok(message):
+    if message.reply_to_message:
+        if message.reply_to_message.from_user.username in Pokemon.pokemons.keys():
+            enemy = Pokemon.pokemons[message.reply_to_message.from_user.username]
+            res = enemy.info()
+            bot.send_message(message.chat.id, res)
+
+@bot.message_handler(commands=['feed'])
+def eat(message):
+    if message.from_user.username in Pokemon.pokemons.keys():
+        pok = Pokemon.pokemons[message.from_user.username]
+        bot.send_message(message.chat.id, pok.feed())
 
 bot.infinity_polling(none_stop=True)
 
